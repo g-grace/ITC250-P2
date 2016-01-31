a9d2a240394188a1db012e05aa8bcfb8febb16cd
w0ng3r <w0ng3r@gmail.com>
2016-01-31 14:47:24 -0800

    Update git_ItemClass.php
    bugfixes

--- git_ItemClass.php
+++ git_ItemClass.php
@@ -2,20 +2,20 @@
 //item class
 
 class Item{
-int $quantity=0
-String $name=''
-String $description=''
-double $price=0.0
+private $quantity=0;
+private $name='';
+private $description='';
+private $price=0.0;
 
     
     public function Item($itemName, $itemDescription, $itemPrice) //class constructor
     {
-        this->name=$itemName;
-        this->description  = $itemDescription;
-        this->price = $itemPrice;
+        $this->name=$itemName;
+        $this->description  = $itemDescription;
+        $this->price = $itemPrice;
     
     }
     
-
+}
 
 ?>
