f540097d186daa47b3338da364ecafeb9d88e51f
w0ng3r <w0ng3r@gmail.com>
2016-01-31 14:48:24 -0800

    Update git_OrderPage.php
    bug and syntax fixes

--- git_OrderPage.php
+++ git_OrderPage.php
@@ -1,9 +1,7 @@
-
-
     <?php
     //item order page
-    include ItemClass;
-    include itemFunctions;
+    include 'git_ItemClass.php';
+    include 'git_ItemFunctions.php';
 
     $menuItems[]=new Item("Tacos", 2.50, "Two Tacos made with your choice of meat. lettuce and guacamole");
     $menuItems[]=new Item("Burrito", 4.50, "Your choice of meat, rice, beans, tomato and grilled onions wrapped in a tortilla skin");
@@ -12,40 +10,43 @@
     $menuItems[]=new Item("Horchata", 2.00, "Wash it down with our delicious, authentic Horchata");
     $menuItems[]=new Item("Sodas", 1.50, "Take your pick of our Mexican sodas!");
     $menuItems[]=new Item("Churros", 1.50, "Fried dough sprinkled with cinnamon and sugar.");
-    <html>
-
-        if (!isset($_POST['orderedItems'] //if nothing is ordered, print the menu
+    ?>
+	<html>
+	<?php
+        $srv = $_SERVER['PHP_SELF'];
+        if (!isset($_POST['orderedItems'])) //if nothing is ordered, print the menu
         { 
-            <form action = "<?php echo $_SERVER[’PHP_SELF’]; ?>"  method = "POST"> 
+            echo '
+            <form action = "' . $srv . '"  method = "POST"> 
 
-                <table>
-                {
-                    while menuItems[]->Item //while the array has items left in it
+                <table>';
+                
+                    foreach($menuItems as $tempItem) //while the array has items left in it
                     {
-                        echo createLineItem(Item); //call create row function and pass in individual item
+                        createLineItem($tempItem); //call create row function and pass in individual item
                     }
-                }
+               
+				echo '
+	        	<input type = "submit" name = "order" value ="Submit Order"> 
+	   	       	</table>
+    	    	</form>   ';
+		 }//if statement to generate menu + order form
 
-         <input type = "submit" name = "order" value ="Submit Order"/> //submit order, calls postback to current page with array of ordered ITems
-         </table>
-         </form>    
-                     }//if form
 
-                     else if(isset($_POST['orderedItems'])//items ordered, display summary and price
-                    {
-                       
-                        foreach ($_POST['orderedItems'] as $tempItem)
-                        {   $tempQuantity = $tempItem->name . "quantity";
-                            $tempItem->quantity = $_POST[$tempQuantity];
-                        }
+		else if(isset($_POST['orderedItems']))//items ordered, display summary and price
+        {
+			foreach ($_POST['orderedItems'] as $tempItem)
+            {   
+				$tempQuantity = $tempItem->name . "quantity";
+				$tempItem->quantity = $_POST[$tempQuantity];
+            }
   
                         //save item quantities each item in array before calling calculate total function
                         //while items exists in ordered items, if item name equals
-                        CalculateTotal($_POST['orderedItems']);
+         CalculateTotal($_POST['orderedItems']);
                    
-                    
-                    }
-if array exists, 
+           }
+ 
 
 
       
